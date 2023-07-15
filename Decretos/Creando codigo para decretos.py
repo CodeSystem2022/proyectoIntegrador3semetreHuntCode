3f743083ab36b85d93a8121c5b2d138ef90bcb9b
Juan Viñas <113565252+JuannMV@users.noreply.github.com>
2023-07-15 20:14:54 -0300

    Creando decreto en Python
    Creamos decreto.py

---
+++ Decretos/Creando codigo para decretos.py
@@ -0,0 +1,24 @@
+def mostrar_menu_decretos():
+    print("*** DECRETOS ****")
+    print("1. Decreto 1338/1996")
+    print("2. Decreto 911/1996")
+    print("3. Decreto 617/1997")
+    print("4. Decreto 249/2007")
+    print("5. Salir")
+    print("*********")
+
+def opcion_decretos_1():
+    print("Regula los servicios de Medicina y de Higiene y Seguridad en el Trabajo, de carácter preventivo.")
+    # Aquí cargamos la opción 1, Decreto 1338/1996
+
+def opcion_decretos_2():
+    print("Reglamento de Higiene y Seguridad para la Industria de la Construcción. Y Resol. SRT 231/96.")
+    # Aquí cargamos la opción 2, Decreto 911/1996
+
+def opcion_decretos_3():
+    print("Reglamento en Higiene y Seguridad en el Trabajo para la Actividad Agraria.")
+    # Aquí cargamos la opción 3, Decreto 617/1997
+
+def opcion_decretos_4():
+    print("Reglamento en Higiene y Seguridad en el Trabajo para la Actividad Minera.")
+    # Aquí cargamos la opción 4, Decreto 249/2007
